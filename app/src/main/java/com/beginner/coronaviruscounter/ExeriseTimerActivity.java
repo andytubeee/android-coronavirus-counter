d2f1f7ebea2aed15e7ee546f4b51161d8c98fac5
andytubeee <andy.tubeee@gmail.com>
2020-03-28 18:15:52 -0400

    March 28th, 6:15pm
    -Point System Fixed
    -Bunch of other bugs

--- app/src/main/java/com/beginner/coronaviruscounter/ExeriseTimerActivity.java
+++ app/src/main/java/com/beginner/coronaviruscounter/ExeriseTimerActivity.java
@@ -1,19 +1,21 @@
 package com.beginner.coronaviruscounter;
 import android.annotation.SuppressLint;
-import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.os.Bundle;
-import android.text.method.DateTimeKeyListener;
-import android.util.Log;
+import android.preference.PreferenceManager;
+import android.text.TextPaint;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,35 +23,42 @@ import android.widget.Toast;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.Stack;
+import java.util.Timer;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import org.joda.time.DateTime;
 
-public class ExeriseTimerActivity extends AppCompatActivity {
+import static com.beginner.coronaviruscounter.Timer_Service.NOTIFY_INTERVAL;
 
-    TextView timer ;
+public class ExeriseTimerActivity extends AppCompatActivity{
+
+    TextView timer, pointsTextView,trackingLog ;
     Button start, pause, reset;
     ListView listView;
     long MillisecondTime, StartTime, TimeBuff, UpdateTime = 0L ;
     Handler handler;
     int Seconds, Minutes, Hours, MilliSeconds ;
-    String[] ListElements = new String[] {  };
-    List<String> ListElementsArrayList;
-    ArrayAdapter<String> adapter;
+
+    boolean RedeemedPoints;
 
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String TEXT = "text";
-    private String text;
+    private String exerciseLogText,pointsText;
+
+
+    SharedPreferences mpref;
+    SharedPreferences.Editor mEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,17 +69,10 @@ public class ExeriseTimerActivity extends AppCompatActivity {
         start = (Button)findViewById(R.id.button);
         pause = (Button)findViewById(R.id.button2);
         reset = (Button)findViewById(R.id.button3);
-        listView = (ListView)findViewById(R.id.listview1);
+        trackingLog=(TextView)findViewById(R.id.trackingLog);
+        pointsTextView = (TextView)findViewById(R.id.pointsText);
 
-        handler = new Handler() ;
-
-        ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
-
-        adapter = new ArrayAdapter<String>(ExeriseTimerActivity.this,
-                R.layout.list_white_text,
-                ListElementsArrayList
-        );
-        listView.setAdapter(adapter);
+        handler = new Handler();
 
         loadData();
         updateViews();
@@ -114,9 +116,6 @@ public class ExeriseTimerActivity extends AppCompatActivity {
                 MilliSeconds = 0 ;
 
                 timer.setText("00:00:00");
-
-                ListElementsArrayList.clear();
-
             }
         });
 
@@ -151,6 +150,7 @@ public class ExeriseTimerActivity extends AppCompatActivity {
 
     };
 
+    @SuppressLint("SetTextI18n")
     public void saveLap(View view) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             String hour = timer.getText().toString().split(":")[0];
@@ -158,36 +158,141 @@ public class ExeriseTimerActivity extends AppCompatActivity {
             String second = timer.getText().toString().split(":")[2];
             DateTime dateTime = new DateTime();
             org.joda.time.LocalDate ld = new org.joda.time.LocalDate();
-            if ((dateTime.toLocalDate()).toString().equals(ListElementsArrayList.toString().split(" ")[0].replace("[",""))){
-                ListElementsArrayList.clear();
-                ListElementsArrayList.add(dateTime.toLocalDate()+" You have exercised for "+hour+" hours "+minute+" minutes "+second+" seconds");
+            if (!dateTime.toLocalDate().toString().equals(trackingLog.toString().split(" ")[0].replace("[", ""))) {
+                if(!trackingLog.getText().toString().equals("")){
+                    String secWorked, minWorked, hWorked;
+                    secWorked = trackingLog.getText().toString().split(" ")[9];
+                    minWorked = trackingLog.getText().toString().split(" ")[7];
+                    hWorked = trackingLog.getText().toString().split(" ")[5];
+
+                    Integer totalS, totalM, totalH;
+                    totalS = Integer.parseInt(second)+Integer.parseInt(secWorked);
+                    totalM = Integer.parseInt(minute)+Integer.parseInt(minWorked);
+                    totalH = Integer.parseInt(hour)+Integer.parseInt(hWorked);
+
+                    totalH = totalM/60;
+                    totalM = totalS/60;
+                    totalS = totalS%60;
+
+                    trackingLog.setText(dateTime.toLocalDate() + " You have exercised for " + totalH.toString() + " hours " + totalM.toString() + " minutes " + totalS.toString() + " seconds");
+                }
+                else{
+                    trackingLog.setText(dateTime.toLocalDate() + " You have exercised for " + hour + " hours " + minute + " minutes " + Integer.parseInt(second) + " seconds");
+                }
                 saveData();
-            }
-            else {
-                ListElementsArrayList.add(dateTime.toLocalDate()+" You have exercised for "+hour+" hours "+minute+" minutes "+second+" seconds");
+                timer.setText("00:00:00");
+                TimeBuff += MillisecondTime;
+                handler.removeCallbacks(runnable);
+                MillisecondTime = 0L ;
+                StartTime = 0L ;
+                TimeBuff = 0L ;
+                UpdateTime = 0L ;
+                Seconds = 0 ;
+                Minutes = 0 ;
+                MilliSeconds = 0 ;
+                start.setEnabled(true);
+            } /*else {
+                String secWorked, minWorked, hWorked;
+                secWorked = trackingLog.getText().toString().split(" ")[9];
+                minWorked = trackingLog.getText().toString().split(" ")[7];
+                hWorked = trackingLog.getText().toString().split(" ")[5];
+
+                Integer totalS, totalM, totalH;
+                totalS = Integer.parseInt(second)+Integer.parseInt(secWorked);
+                totalM = Integer.parseInt(minute)+Integer.parseInt(minWorked);
+                totalH = Integer.parseInt(hour)+Integer.parseInt(hWorked);
+
+                trackingLog.setText(dateTime.toLocalDate() + " You have exercised for " + totalH.toString() + " hours " + totalM.toString() + " minutes " + totalS.toString() + " seconds");
                 saveData();
-            }
+            }*/
 
         }
-
-        adapter.notifyDataSetChanged();
     }
+    public static final String POINT_KEY = "savePoint";
+    public static final String RED_BOOL_KEY = "saveRedeemedBoolean";
+    public static final String MILLI_KEY = "saveMillisecondLeft";
 
     public void saveData() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        editor.putString(TEXT, ListElementsArrayList.toString().replace("[,","").replace("]",""));
-        editor.apply();
 
-        //Toast.makeText(getApplicationContext(),"Data saved", Toast.LENGTH_LONG).show();
+        editor.putString(TEXT, trackingLog.getText().toString().replace("[,","").replace("]",""));
+        editor.putBoolean(RED_BOOL_KEY,RedeemedPoints);
+        editor.putString(POINT_KEY, pointsTextView.getText().toString());
+
+        editor.apply();
     }
     public void loadData(){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        text = sharedPreferences.getString(TEXT,"");
+        exerciseLogText = sharedPreferences.getString(TEXT,"");
+        RedeemedPoints = sharedPreferences.getBoolean(RED_BOOL_KEY,RedeemedPoints);
+        pointsText = sharedPreferences.getString(POINT_KEY,pointsText);
     }
     public void updateViews(){
-        text.replace("[","");
-        ListElementsArrayList.add(text.replace(",","\n").replace("[",""));
+        exerciseLogText.replace("[","");
+        trackingLog.setText(exerciseLogText.replace(",","\n").replace("[",""));
+        if(pointsText!=null){
+            pointsTextView.setText(pointsText);
+        }
+        else {
+            pointsTextView.setText("Points: 0");
+        }
+    }
+
+    public void popNoti(View view) {
+        Toast.makeText(getApplicationContext(), "1 Point per Every 30 Minutes of Exercise", Toast.LENGTH_LONG).show();
+    }
+
+    protected void nextDay() {
+        super.onResume();
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        int lastTimeStarted = settings.getInt("last_time_started", -1);
+        Calendar calendar = Calendar.getInstance();
+        int today = calendar.get(Calendar.DAY_OF_YEAR);
+
+        if (today != lastTimeStarted) {
+            if(RedeemedPoints)
+                RedeemedPoints = false;
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("last_time_started", today);
+            editor.commit();
+        }
+    }
+
+    @SuppressLint({"SetTextI18n", "DefaultLocale"})
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void redeemPoints(View view) {
+        try{
+        Integer seconds, minutes,hours;
+        seconds = Integer.parseInt(trackingLog.getText().toString().split(" ")[9]);
+        minutes = Integer.parseInt(trackingLog.getText().toString().split(" ")[7]);
+        hours = Integer.parseInt(trackingLog.getText().toString().split(" ")[5]);
+        if(!RedeemedPoints&&seconds>=5){
+            Integer totalPoints = ((seconds)+(minutes*60)+(hours*3600))/5;
+            pointsTextView.setText("Points: "+totalPoints.toString());
+            trackingLog.setText(new DateTime().toLocalDate() + " You have exercised for 0 hours 0 minutes "+((seconds)+(minutes*60)+(hours*3600))%5+" seconds");
+            Toast.makeText(this,"You can only redeem points once a day",Toast.LENGTH_SHORT).show();
+            RedeemedPoints = true;
+            saveData();
+            nextDay();
+        }
+        else if(RedeemedPoints){
+            Toast.makeText(this,"Please wait Tomorrow to Redeem your points",Toast.LENGTH_SHORT).show();
+        }
+
+        if(seconds<5&&!RedeemedPoints){
+            Toast.makeText(this,"More time needed to redeem",Toast.LENGTH_SHORT).show();
+        }
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(this,"If this shows up, something is probably wrong and I am not fixing it",Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void startGiftCardActivity(View view) {
+
     }
 }
